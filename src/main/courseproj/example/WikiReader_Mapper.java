bc6df3c14652e4bd45143e64200e773e41126013
Josh Bradley <jgbradley1@gmail.com>
2013-04-15 23:18:45 -0400

    updated WikiReader example…still not working

--- src/main/courseproj/example/WikiReader_Mapper.java
+++ src/main/courseproj/example/WikiReader_Mapper.java
@@ -9,16 +9,20 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 // Mapper: emits (token, 1) for every article occurrence.
-public class WikiReader_Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class WikiReader_Mapper extends Mapper<Text, Text, Text, IntWritable> {
 
   // Reuse objects to save overhead of object creation.
   private final static Text KEY = new Text();
   private final static IntWritable VALUE = new IntWritable(1);
 
   @Override
-  public void map(LongWritable key, Text value, Context context)
+  public void map(Text key, Text value, Context context)
       throws IOException, InterruptedException {
-    String line = ((Text) value).toString();
+    String keyy = key.toString();
+    String valuee = value.toString();
+    
+    System.out.println("\n\nKEY: " + keyy + "\n\n");
+    System.out.println("\n\nVALUE: " + valuee + "\n\n");
     KEY.set("article count");
     context.write(KEY, VALUE);
   }
