4058593ed14605666e9c090bdbb3421a9837e5f5
Josh Bradley <jgbradley1@gmail.com>
2013-04-16 23:38:02 -0400

    Fixed some example code

--- src/main/courseproj/example/WikiReader_Mapper.java
+++ src/main/courseproj/example/WikiReader_Mapper.java
@@ -3,19 +3,36 @@ package courseproj.example;
 import java.io.IOException;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
 
 // Mapper: emits (token, 1) for every article occurrence.
-public class WikiReader_Mapper extends Mapper<Text, Text, Text, IntWritable> {
+public class WikiReader_Mapper implements Mapper<Text, Text, Text, IntWritable> {
 
-  // Reuse objects to save overhead of object creation.
-  private final static Text KEY = new Text();
-  private final static IntWritable VALUE = new IntWritable(1);
+    // Reuse objects to save overhead of object creation.
+    private final static Text KEY = new Text();
+    private final static IntWritable VALUE = new IntWritable(1);
 
-  @Override
-  public void map(Text key, Text value, Context context)
-      throws IOException, InterruptedException {
-    KEY.set("article count");
-    context.write(KEY, VALUE);
-  }
+    @Override
+    public void map(Text key, Text arvalueg1, OutputCollector<Text, IntWritable> collector, Reporter reporter)
+            throws IOException {
+        // TODO Auto-generated method stub
+        KEY.set("article count");
+        collector.collect(KEY, VALUE);
+    }
+    
+    
+    @Override
+    public void configure(JobConf arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void close() throws IOException {
+        // TODO Auto-generated method stub
+        
+    }
 }
--- src/main/courseproj/example/WikiReader_Reducer.java
+++ src/main/courseproj/example/WikiReader_Reducer.java
@@ -5,28 +5,44 @@ import java.util.Iterator;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
 
 
 //Reducer: sums up all the counts.
-public class WikiReader_Reducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+public class WikiReader_Reducer implements Reducer<Text, IntWritable, Text, IntWritable> {
 
-  // Reuse objects.
-  private final static IntWritable SUM = new IntWritable();
+    // Reuse objects.
+    private final static IntWritable SUM = new IntWritable();
 
-  @Override
-  public void reduce(Text key, Iterable<IntWritable> values, Context context)
-      throws IOException, InterruptedException {
-    // Sum up values.
-    Iterator<IntWritable> iter = values.iterator();
-    int sum = 0;
-    while (iter.hasNext()) {
-      sum += iter.next().get();
+    @Override
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> collector,
+            Reporter reporter) throws IOException {
+        
+        int sum = 0;
+        while (values.hasNext()) {
+            sum += values.next().get();
+        }
+
+        System.out.println("\n\nTotal Count: " + sum);
+
+        SUM.set(sum);
+        collector.collect(key, SUM);
+
     }
     
-    System.out.println("\n\nTotal Count: " + sum);
     
-    SUM.set(sum);
-    context.write(key, SUM);
-  }
+    @Override
+    public void configure(JobConf arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        // TODO Auto-generated method stub
+
+    }
 }
