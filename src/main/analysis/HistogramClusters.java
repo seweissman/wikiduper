daf44fdd2922a9da8026386691694191f376b8be
Sarah Weissman <seweissman@gmail.com>
2013-05-08 00:02:14 -0400

    Added unique title/sentence histograms

--- src/main/analysis/HistogramClusters.java
+++ src/main/analysis/HistogramClusters.java
@@ -27,9 +27,13 @@ public class HistogramClusters {
 		FileInputStream fin;
 		
 		TreeMap<Integer,Integer> histogram = new TreeMap<Integer,Integer>();
+		TreeMap<Integer,Integer> titlehistogram = new TreeMap<Integer,Integer>();
+		TreeMap<Integer,Integer> sentencehistogram = new TreeMap<Integer,Integer>();
 		HashSet<String> articleset = new HashSet<String>();
 		HashSet<String> sentenceset = new HashSet<String>();
 		ArrayList<String> cluster = new ArrayList<String>();
+		HashSet<String> clustertitles = new HashSet<String>();
+		HashSet<String> clustersentences = new HashSet<String>();
         int clusterct = 0;
         int sentencect = 0;
 		try {
@@ -67,27 +71,50 @@ public class HistogramClusters {
 	                    if(clusterct % 10000 == 0) System.err.println("clusterct = " + clusterct);
 
 	                    int size = cluster.size();
-	                    if(!histogram.containsKey(size)){
-	                        histogram.put(size, 0);    
-	                    }
-	                    histogram.put(size, histogram.get(size) + 1);
-	                    /*
+                        if(!histogram.containsKey(size)){
+                            histogram.put(size, 0);    
+                        }
+                        histogram.put(size, histogram.get(size) + 1);
+	                    
+                        size = clustertitles.size();
+                        if(!titlehistogram.containsKey(size)){
+                            titlehistogram.put(size, 0);    
+                        }
+                        titlehistogram.put(size, titlehistogram.get(size) + 1);
+                        
+                        size = clustersentences.size();
+                        if(!sentencehistogram.containsKey(size)){
+                            sentencehistogram.put(size, 0);    
+                        }
+                        sentencehistogram.put(size, sentencehistogram.get(size) + 1);
+	                    
+                        /*
 	                    if(cluster.size() > 10000){
                             int ct = 0;
+                            System.out.println("Cluster size: " + cluster.size());
+                            System.out.println("N unique sentences " +  clustersentences.size());
+                            System.out.println("N unique titles " +  clustertitles.size());
+
                             for(String l : cluster){
-	                            if(ct > 20) break;
+	                            if(ct > 10) break;
                                 System.out.println(l);
 	                            ct++;
 	                        }
+                            System.out.println();
 	                    }
 	                    */
 	                    
 	                    cluster.clear();
+	                    clustersentences.clear();
+	                    clustertitles.clear();
                         clusterct++;
 	                }
 	                
 	                clustcurr = clust;
                     cluster.add(line);
+                    clustersentences.add(sentence);
+                    clustertitles.add(title);
+                    
                     articleset.add(title);
                     sentenceset.add(sentence);
 	            }
@@ -106,9 +133,21 @@ public class HistogramClusters {
 		System.out.println("N clusters: " + clusterct);            
 		System.out.println("N unique articles: " + articleset.size());
 		System.out.println("N unique sentences: " + sentenceset.size());
-            for(int b : histogram.keySet()){
-                System.out.println(b + "\t" + histogram.get(b));
-            }
+        
+		System.out.println("Cluster histogram");
+        for(int b : histogram.keySet()){
+            System.out.println(b + "\t" + histogram.get(b));
+        }
+        
+        System.out.println("Unique Title histogram");
+        for(int b : titlehistogram.keySet()){
+            System.out.println(b + "\t" + titlehistogram.get(b));
+        }
+        
+        System.out.println("Uniqe Sentence histogram");
+        for(int b : sentencehistogram.keySet()){
+            System.out.println(b + "\t" + sentencehistogram.get(b));
+        }
 
 	}
 
