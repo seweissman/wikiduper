8ff06e7402352550da97f4b5fddd85e7a71bcb59
Sarah Weissman <seweissman@gmail.com>
2013-06-07 12:35:55 -0400

    Adding echo statements to script

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -9,12 +9,21 @@ lang=en
 # n $6
 # shingleLen $7
 
+echo etc/hadoop-cluster.sh courseproj.application.MinhashWikipediaPages -wiki_language $lang -input $1 -output enwiki-20130503-pairs -numReducers $nReducers -nHash $3 -k $5 -n $6 -bits $4 -shingleLen $7
 etc/hadoop-cluster.sh courseproj.application.MinhashWikipediaPages -wiki_language $lang -input $1 -output enwiki-20130503-pairs -numReducers $nReducers -nHash $3 -k $5 -n $6 -bits $4 -shingleLen $7
+echo etc/hadoop-cluster.sh courseproj.application.DedupSentencePairs -input enwiki-20130503-pairs -output enwiki-20130503-pairsdedup
 etc/hadoop-cluster.sh courseproj.application.DedupSentencePairs -input enwiki-20130503-pairs -output enwiki-20130503-pairsdedup
+echo rm -rf enwiki-20130503-pairsdedup
 rm -rf enwiki-20130503-pairsdedup
+echo hadoop fs -get enwiki-20130503-pairsdedup
 hadoop fs -get enwiki-20130503-pairsdedup
+echo hadoop fs -rm -r ewiki-20130503-clusters
 hadoop fs -rm -r ewiki-20130503-clusters
+echo etc/run.sh courseproj.application.MergeClusters -pairfile enwiki-20130503-pairsdedup -output enwiki-20130503-clusters
 etc/run.sh courseproj.application.MergeClusters -pairfile enwiki-20130503-pairsdedup -output enwiki-20130503-clusters
+echo hadoop fs -put ewiki-20130503-clusters
 hadoop fs -put ewiki-20130503-clusters
+echo etc/hadoop-cluster.sh courseproj.application.GetSentenceClusters -input $1 -wiki_language en -clustermap enwiki-20130503-clusters -output $2 -numReducers $nReducers
 etc/hadoop-cluster.sh courseproj.application.GetSentenceClusters -input $1 -wiki_language en -clustermap enwiki-20130503-clusters -output $2 -numReducers $nReducers
+echo hadoop fs -get $2
 hadoop fs -get $2
