3d156ab22ccc4b59004b78ab06d6b8ff2f72343c
Sarah Weissman <seweissman@gmail.com>
2013-06-28 10:32:44 -0400

    Some changes to hash function.

--- src/main/wikiduper/hash/MultiplyShiftHash.java
+++ src/main/wikiduper/hash/MultiplyShiftHash.java
@@ -47,11 +47,12 @@ public class MultiplyShiftHash {
       long sum = 0;
       for(int i=0;i<v.length; i++){
         long a = r.nextLong();
+        long b = r.nextLong();
         if(a%2 == 0){
           // make sure a is odd (better way to do this?)
           a += 1;
         }
-        sum += v[i]*a;
+        sum += v[i]*a + b;
       }
       hashvec[s] = sum >>> (64 - m);
 
@@ -59,24 +60,80 @@ public class MultiplyShiftHash {
     return hashvec;
   }
   
+  public long hash(long []v, int i){
+
+      Random r = new Random(seeds[i]);
+
+      long sum = 0;
+      for(int j=0;j<v.length; j++){
+        long a = r.nextLong();
+        long b = r.nextLong();
+        if(a%2 == 0){
+          // make sure a is odd (better way to do this?)
+          a += 1;
+        }
+        sum += v[j]*a + b;
+      }
+      return (sum >>> (64 - m));
+
+    }
+    
+  
   /**
    * Hashes a variable length string input
    * 
    * @param str
    * @return
    */
-  public long[] hash(String str){
+public long[] hash1(String str){
     byte b[] = str.getBytes();
     long[] v = new long[8*b.length/Long.SIZE + 1];
     //System.out.println("n bytes = " + b.length);
     //System.out.println("n long = " + v.length);
+    
     for(int i=0;i<b.length;i++){
-      v[i/Long.SIZE] |= (b[i] & 0xff) << (8*(i%Long.SIZE));
+      //v[i/Long.SIZE] |= (b[i] & 0xff) << (8*(i%Long.SIZE));
+        v[i/Long.SIZE] |= (Byte.valueOf(b[i]) & 0xff) << (8*(i%Long.SIZE));
     }
     
+    //return hash(hash(v));
     return hash(v);
   }
+
+long radix = 128;
+
+
+public long[] hash(String str){
+  byte b[] = str.getBytes();
+  int longbytes = Long.SIZE/8;
+  long[] v = new long[8*b.length/longbytes + 1];
+  //System.out.println("n bytes = " + b.length);
+  //System.out.println("n long = " + v.length);
   
+  for(int i=0;i<b.length;i++){
+    //v[i/Long.SIZE] |= (b[i] & 0xff) << (8*(i%Long.SIZE));
+      v[i/longbytes] |= (Byte.valueOf(b[i]) & 0xff) << (longbytes*(i%longbytes));
+  }
+  //128 = 2^7 128^8 = 2^58
+          
+  //return hash(hash(v));
+  return hash(v);
+}
+
+
+public long hash(String str, int i){
+    byte b[] = str.getBytes();
+    long[] v = new long[8*b.length/Long.SIZE + 1];
+    //System.out.println("n bytes = " + b.length);
+    //System.out.println("n long = " + v.length);
+    for(int j=0;j<b.length;j++){
+        //v[j/Long.SIZE] |= (b[j] & 0xff) << (8*(j%Long.SIZE));
+        v[j/Long.SIZE] |= (Byte.valueOf(b[j]).hashCode() & 0xff) << (8*(j%Long.SIZE));
+    }
+    
+    return hash(v,i);
+    }
+
   public static void hashlongtest(MultiplyShiftHash hashfamily){
 
     for(int v=0; v<256; v++){
@@ -99,20 +156,20 @@ public class MultiplyShiftHash {
   
   
   public static void hashstringtest(MultiplyShiftHash hashfamily){
-    String stest = "Determined to expose the person who drugged and assaulted her at a party the year before, Veronica questions her friends and many rivals about the events that occurred that night at Shelly Pomeroy's (MELISSA HOOVER) house. Meanwhile, when one of Veronica and Wallace's stunts affects Alicia's (recurring guest star ERICA GIMPEL) job at Kane Software, she confronts Keith about his daughter's behavior. Later, in an attempt to get closer to his son, Aaron (recurring guest star HARRY HAMLIN) hosts a birthday party for Logan that surprises everyone.";
+    String stest = "Determined to expose exposed exposing the person who drugged and assaulted her at a party the year before, Veronica questions her friends and many rivals about the events that occurred that night at Shelly Pomeroy's (MELISSA HOOVER) house. Meanwhile, when one of Veronica and Wallace's stunts affects Alicia's (recurring guest star ERICA GIMPEL) job at Kane Software, she confronts Keith about his daughter's behavior. Later, in an attempt to get closer to his son, Aaron (recurring guest star HARRY HAMLIN) hosts a birthday party for Logan that surprises everyone.";
     String stestsplit[] = stest.split(" "); 
     System.out.println("Split length " + stestsplit.length);
     for(int i=0; i<stestsplit.length; i++){
       String s = stestsplit[i];
             
       long hashv[] = hashfamily.hash(s);
-      //System.out.print(s + ",");
-      //System.out.print("[");
+      System.out.print(s + ",");
+      System.out.print("[");
       for(int j=0;j<hashv.length;j++){
-        //if(j != 0) System.out.print(", ");
-        System.out.println(hashv[j]);
+        if(j != 0) System.out.print(", ");
+        System.out.print(hashv[j]);
       }
-      //System.out.println("]");
+      System.out.println("]");
     }
   }
   
