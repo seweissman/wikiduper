5b8e5b4d7ab71f56b666bbe8b4409712e8bc2751
Sarah Weissman <seweissman@gmail.com>
2013-07-05 23:26:22 -0400

    Attempting to make classes for preprocessing generic text collections

---
+++ src/main/wikiduper/clir/rp/RepackText.java
@@ -0,0 +1,220 @@
+/*
+ * Cloud9: A MapReduce Library for Hadoop
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package wikiduper.clir.rp;
+
+import java.io.IOException;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+
+import edu.umd.cloud9.collection.line.TextDocument;
+
+/**
+ * Tool for repacking Text dumps into <code>SequenceFiles</code>.
+ *
+ * @author Jimmy Lin
+ * @author Peter Exner
+ */
+public class RepackText extends Configured implements Tool {
+  private static final Logger LOG = Logger.getLogger(RepackText.class);
+
+  private static enum Records { TOTAL };
+
+  private static class MyMapper extends 
+      Mapper<LongWritable, Text, IntWritable, TextDocument> {
+    private static final IntWritable docno = new IntWritable();
+    private static final TextDocnoMapping docnoMapping = new TextDocnoMapping();
+    
+    @Override
+    public void setup(Context context) {
+      try {
+        Path p = new Path(context.getConfiguration().get(DOCNO_MAPPING_FIELD));
+        LOG.info("Loading docno mapping: " + p);
+
+        FileSystem fs = FileSystem.get(context.getConfiguration());
+        if (!fs.exists(p)) {
+          throw new RuntimeException(p + " does not exist!");
+        }
+
+        docnoMapping.loadMapping(p, fs);
+      } catch (Exception e) {
+        throw new RuntimeException("Error loading docno mapping data file!");
+      }
+    }
+
+    @Override
+    public void map(LongWritable key, Text doc, Context context)
+        throws IOException, InterruptedException {
+      context.getCounter(Records.TOTAL).increment(1);
+      String id = key.toString();
+      if (id != null) {
+        // We're going to discard pages that aren't in the docno mapping.
+        int n = docnoMapping.getDocno(id);
+        if (n >= 0) {
+          docno.set(n);
+          TextDocument textdoc = new TextDocument();
+          textdoc.setDocid(key.toString());
+          textdoc.setContent(doc.toString());
+          context.write(docno, textdoc);
+        }
+      }
+    }
+  }
+
+  private static final String DOCNO_MAPPING_FIELD = "DocnoMappingDataFile";
+
+  private static final String INPUT_OPTION = "input";
+  private static final String OUTPUT_OPTION = "output";
+  private static final String MAPPING_FILE_OPTION = "mapping_file";
+  private static final String COMPRESSION_TYPE_OPTION = "compression_type";
+  private static final String LANGUAGE_OPTION = "wiki_language";
+
+  @SuppressWarnings("static-access") @Override
+  public int run(String[] args) throws Exception {
+    Options options = new Options();
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("XML dump file").create(INPUT_OPTION));
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("output location").create(OUTPUT_OPTION));
+    options.addOption(OptionBuilder.withArgName("path").hasArg()
+        .withDescription("mapping file").create(MAPPING_FILE_OPTION));
+    options.addOption(OptionBuilder.withArgName("block|record|none").hasArg()
+        .withDescription("compression type").create(COMPRESSION_TYPE_OPTION));
+    options.addOption(OptionBuilder.withArgName("en|sv|de").hasArg()
+        .withDescription("two-letter language code").create(LANGUAGE_OPTION));
+
+    CommandLine cmdline;
+    CommandLineParser parser = new GnuParser();
+    try {
+      cmdline = parser.parse(options, args);
+    } catch (ParseException exp) {
+      System.err.println("Error parsing command line: " + exp.getMessage());
+      return -1;
+    }
+
+    if (!cmdline.hasOption(INPUT_OPTION) || !cmdline.hasOption(OUTPUT_OPTION) ||
+        !cmdline.hasOption(MAPPING_FILE_OPTION) || !cmdline.hasOption(COMPRESSION_TYPE_OPTION)) {
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp(this.getClass().getName(), options);
+      ToolRunner.printGenericCommandUsage(System.out);
+      return -1;
+    }
+
+    String inputPath = cmdline.getOptionValue(INPUT_OPTION);
+    String outputPath = cmdline.getOptionValue(OUTPUT_OPTION);
+    String mappingFile = cmdline.getOptionValue(MAPPING_FILE_OPTION);
+    String compressionType = cmdline.getOptionValue(COMPRESSION_TYPE_OPTION);
+
+    if (!"block".equals(compressionType) && !"record".equals(compressionType) && !"none".equals(compressionType)) {
+      System.err.println("Error: \"" + compressionType + "\" unknown compression type!");
+      return -1;
+    }
+
+    String language = null;
+    if (cmdline.hasOption(LANGUAGE_OPTION)) {
+      language = cmdline.getOptionValue(LANGUAGE_OPTION);
+      if(language.length()!=2){
+        System.err.println("Error: \"" + language + "\" unknown language!");
+        return -1;
+      }
+    }
+
+    // this is the default block size
+    int blocksize = 1000000;
+
+    Job job = Job.getInstance(getConf());
+    job.setJarByClass(RepackText.class);
+    job.setJobName(String.format("RepackText[%s: %s, %s: %s, %s: %s, %s: %s]",
+        INPUT_OPTION, inputPath, OUTPUT_OPTION, outputPath, COMPRESSION_TYPE_OPTION,
+        compressionType, LANGUAGE_OPTION, language));
+
+    job.getConfiguration().set(DOCNO_MAPPING_FIELD, mappingFile);
+
+    LOG.info("Tool name: " + this.getClass().getName());
+    LOG.info(" - XML dump file: " + inputPath);
+    LOG.info(" - output path: " + outputPath);
+    LOG.info(" - docno mapping data file: " + mappingFile);
+    LOG.info(" - compression type: " + compressionType);
+    LOG.info(" - language: " + language);
+
+    if ("block".equals(compressionType)) {
+      LOG.info(" - block size: " + blocksize);
+    }
+
+    job.setNumReduceTasks(0);
+
+    SequenceFileInputFormat.addInputPath(job, new Path(inputPath));
+    SequenceFileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+    if ("none".equals(compressionType)) {
+      SequenceFileOutputFormat.setCompressOutput(job, false);
+    } else {
+      SequenceFileOutputFormat.setCompressOutput(job, true);
+
+      if ("record".equals(compressionType)) {
+        SequenceFileOutputFormat.setOutputCompressionType(job, SequenceFile.CompressionType.RECORD);
+      } else {
+        SequenceFileOutputFormat.setOutputCompressionType(job, SequenceFile.CompressionType.BLOCK);
+        job.getConfiguration().setInt("io.seqfile.compress.blocksize", blocksize);
+      }
+    }
+
+    if (language != null) {
+      job.getConfiguration().set("wiki.language", language);
+    }
+
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(SequenceFileOutputFormat.class);
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(TextDocument.class);
+
+    job.setMapperClass(MyMapper.class);
+
+    // Delete the output directory if it exists already.
+    FileSystem.get(getConf()).delete(new Path(outputPath), true);
+
+    job.waitForCompletion(true);
+
+    return 0;
+  }
+
+  public RepackText() {}
+
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new RepackText(), args);
+  }
+}
---
+++ src/main/wikiduper/clir/rp/TextDocnoMapping.java
@@ -0,0 +1,204 @@
+/*
+ * Cloud9: A Hadoop toolkit for working with big data
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package wikiduper.clir.rp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.util.LineReader;
+import org.apache.log4j.Logger;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+
+import edu.umd.cloud9.collection.DocnoMapping;
+
+/**
+ * Object that maps between docids (String identifiers) to docnos (sequentially-numbered 
+ * ints). The {@code main} of this class provides a simple program for accessing docno mappings.
+ * Command-line arguments are as follows:
+ *
+ * <ul>
+ * <li>list, getDocno, getDocid: the command&mdash;list all mappings; get docno from docid; or, get
+ * docid from docno</li>
+ * <li>[mappings-file]: the mappings file</li>
+ * <li>[docid/docno]: the docid or docno (optional)</li>
+ * </ul>
+ *
+ * @author Jimmy Lin
+ */
+public class TextDocnoMapping implements DocnoMapping {
+  private static final Logger LOG = Logger.getLogger(TextDocnoMapping.class);
+
+  private HashMap<Integer,String> docno_docidmap = new HashMap<Integer,String>();
+  private HashMap<String,Integer> docid_docnomap = new HashMap<String,Integer>();
+
+  /**
+   * Creates a {@code GenericcDocnoMapping} object.
+   */
+  public TextDocnoMapping() {}
+
+  @Override
+  public int getDocno(String docid) {
+    Preconditions.checkNotNull(docid);
+    return docid_docnomap.get(docid);
+  }
+
+  @Override
+  public String getDocid(int docno) {
+    return docno_docidmap.get(docno);
+  }
+
+  @Override
+  public void loadMapping(Path p, FileSystem fs) throws IOException {
+    Preconditions.checkNotNull(p);
+    Preconditions.checkNotNull(fs);
+    TextDocnoMapping.readMappingData(p, fs, docid_docnomap, docno_docidmap);
+  }
+
+  /**
+   * Creates a mappings file from the contents of a flat text file containing docid to docno
+   * mappings. This method is used by {@link TrecDocnoMappingBuilder} internally.
+   *
+   * @param input flat text file containing docid to docno mappings
+   * @param output output mappings data file
+   * @param fs {@code FileSystem} to write to
+   * @throws IOException
+   */
+  static public void writeMappingData(Path input, Path output, FileSystem fs, HashMap<String,Integer> docid_docnomap, HashMap<Integer, String> docno_docidmap) throws IOException {
+    Preconditions.checkNotNull(input);
+    Preconditions.checkNotNull(output);
+    Preconditions.checkNotNull(fs);
+
+    LOG.info("Writing docno data to " + output);
+    LineReader reader = new LineReader(fs.open(input));
+    List<String> list = Lists.newArrayList();
+
+    LOG.info("Reading " + input);
+    int cnt = 0;
+    Text line = new Text();
+    while (reader.readLine(line) > 0) {
+      String[] arr = line.toString().split("\\t");
+      list.add(arr[0]);
+      cnt++;
+      if (cnt % 100000 == 0) {
+        LOG.info(cnt + " docs");
+      }
+    }
+    reader.close();
+    LOG.info(cnt + " docs total. Done!");
+
+    cnt = 0;
+    LOG.info("Writing " + output);
+    FSDataOutputStream out = fs.create(output, true);
+    out.writeInt(list.size());
+    for (int i = 0; i < list.size(); i++) {
+      out.writeUTF(list.get(i));
+      cnt++;
+      if (cnt % 100000 == 0) {
+        LOG.info(cnt + " docs");
+      }
+    }
+    out.close();
+    LOG.info(cnt + " docs total. Done!");
+  }
+
+  /**
+   * Reads a mappings file into memory.
+   *
+   * @param p path to the mappings data file
+   * @param fs appropriate FileSystem
+   * @return an array of docids; the index position of each docid is its docno
+   * @throws IOException
+   */
+  static public void readMappingData(Path p, FileSystem fs, HashMap<String,Integer> docid_docnomap, HashMap<Integer, String> docno_docidmap) throws IOException {
+    Preconditions.checkNotNull(p);
+    Preconditions.checkNotNull(fs);
+
+    FSDataInputStream in = fs.open(p);
+    in.readInt();
+    int i = 1;
+    System.out.println("europarl path = " + p);
+    String docid;
+    try{
+    while((docid = in.readUTF()) != null){
+        System.out.println("docid = " + docid + ", docno = " + i + "\n");
+        docid_docnomap.put(docid, i);
+        docno_docidmap.put(i,docid);
+        i++;
+    }}catch(Exception e){
+        in.close();
+    }
+
+  }
+
+  @Override
+  public Builder getBuilder() {
+    return new TextDocnoMappingBuilder();
+  }
+
+  /**
+   * Simple program the provides access to the docno/docid mappings.
+   */
+  public static void main(String[] args) throws IOException {
+    if (args.length < 2) {
+      System.out.println("usage: (list|getDocno|getDocid) [mapping-file] [docid/docno]");
+      System.exit(-1);
+    }
+
+    Configuration conf = new Configuration();
+    FileSystem fs = FileSystem.get(conf);
+
+    System.out.println("loading mapping file " + args[1]);
+    TextDocnoMapping mapping = new TextDocnoMapping();
+    mapping.loadMapping(new Path(args[1]), fs);
+
+    if (args[0].equals("list")) {
+      for (int i = 1; i < mapping.docno_docidmap.size(); i++) {
+        System.out.println(i + "\t" + mapping.docno_docidmap.get(i));
+      }
+    } else if (args[0].equals("getDocno")) {
+      System.out.println("looking up docno for \"" + args[2] + "\"");
+      int idx = mapping.getDocno(args[2]);
+      if (idx > 0) {
+        System.out.println(mapping.getDocno(args[2]));
+      } else {
+        System.err.print("Invalid docid!");
+      }
+    } else if (args[0].equals("getDocid")) {
+      try {
+        System.out.println("looking up docid for " + args[2]);
+        System.out.println(mapping.getDocid(Integer.parseInt(args[2])));
+      } catch (Exception e) {
+        System.err.print("Invalid docno!");
+      }
+    } else {
+      System.out.println("Invalid command!");
+      System.out.println("usage: (list|getDocno|getDocid) [mappings-file] [docid/docno]");
+    }
+  }
+}
