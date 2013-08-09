c724dadeeced5127ceecfbfe8a76db4f4a4e00ed
Sarah Weissman <seweissman@gmail.com>
2013-08-09 08:59:37 -0400

    Added clustering code to wikiclir script

--- scripts/runallclirwiki.sh
+++ scripts/runallclirwiki.sh
@@ -35,15 +35,18 @@ k=$8
 n=$9
 nhash=${10}
 
-output=${11}
+mhoutput=${11}.mh
+mergeoutput=${11}.clusters
+sentenceoutput=${11}.sentences
 
-#echo "etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.PreprocessWikiInput -elang ${elang} -ewiki ${epack} -flang ${flang} -fwiki ${fpack} -eout ${epreproc} -fout ${fpreproc}"
-#etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.PreprocessWikiInput -elang ${elang} -ewiki ${epack} -flang ${flang} -fwiki ${fpack} -eout ${epreproc} -fout ${fpreproc}
-echo "etc/hadoop-cluster.sh wikiduper.clir.minhash.PreprocessTextInput -elang ${elang} -ein ${ein} -flang ${flang} -fin ${fin} -eout ${epreproc} -fout ${fpreproc}"
-etc/hadoop-cluster.sh wikiduper.clir.minhash.PreprocessTextInput -elang ${elang} -ein ${ein} -flang ${flang} -fin ${fin} -eout ${epreproc} -fout ${fpreproc}
-echo "etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.SampleSentenceTranslations -fVocabSrc ${fvocabsrc} -fVocabTgt ${fvocabtgt} -eVocabSrc ${evocabsrc} -eVocabTgt ${evocabtgt} -e2fprobs ${e2fprobs} -f2eprobs ${f2eprobs} -fLang ${flang} -eLang ${elang} -fStopWords ${fstopwords} -eStopWords ${estopwords} -fTokens ${ftokens} -eTokens ${etokens} -ein ${epreproc} -fin ${fpreproc} -output ${sentencemap} -M ${samples}"
-etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.SampleSentenceTranslations -fVocabSrc ${fvocabsrc} -fVocabTgt ${fvocabtgt} -eVocabSrc ${evocabsrc} -eVocabTgt ${evocabtgt} -e2fprobs ${e2fprobs} -f2eprobs ${f2eprobs} -fLang ${flang} -eLang ${elang} -fStopWords ${fstopwords} -eStopWords ${estopwords} -fTokens ${ftokens} -eTokens ${etokens} -ein ${epreproc} -fin ${fpreproc} -output ${sentencemap} -M ${samples}
-echo "etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.MinhashCLIRTest -bits ${bits} -input ${sentencemap} -k ${k} -M ${samples} -n ${n} -nHash ${nhash} -numReducers 20 -output ${output}"
-etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.MinhashCLIRTest -bits ${bits} -input ${sentencemap} -k ${k} -M ${samples} -n ${n} -nHash ${nhash} -numReducers 20 -output ${output}
-
+echo "etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.PreprocessWikiInput -elang ${elang} -ewiki ${epack} -flang ${flang} -fwiki ${fpack} -eout ${epreproc} -fout ${fpreproc}"
+etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.PreprocessWikiInput -elang ${elang} -ewiki ${epack} -flang ${flang} -fwiki ${fpack} -eout ${epreproc} -fout ${fpreproc}
+#echo "etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.SampleSentenceTranslations -fVocabSrc ${fvocabsrc} -fVocabTgt ${fvocabtgt} -eVocabSrc ${evocabsrc} -eVocabTgt ${evocabtgt} -e2fprobs ${e2fprobs} -f2eprobs ${f2eprobs} -fLang ${flang} -eLang ${elang} -fStopWords ${fstopwords} -eStopWords ${estopwords} -fTokens ${ftokens} -eTokens ${etokens} -ein ${epreproc} -fin ${fpreproc} -output ${sentencemap} -M ${samples}"
+#etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.SampleSentenceTranslations -fVocabSrc ${fvocabsrc} -fVocabTgt ${fvocabtgt} -eVocabSrc ${evocabsrc} -eVocabTgt ${evocabtgt} -e2fprobs ${e2fprobs} -f2eprobs ${f2eprobs} -fLang ${flang} -eLang ${elang} -fStopWords ${fstopwords} -eStopWords ${estopwords} -fTokens ${ftokens} -eTokens ${etokens} -ein ${epreproc} -fin ${fpreproc} -output ${sentencemap} -M ${samples}
+echo "etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.MinhashCLIR -bits ${bits} -k ${k} -M ${samples} -n ${n} -nHash ${nhash} -numReducers 20 -output ${output} -fVocabSrc ${fvocabsrc} -fVocabTgt ${fvocabtgt} -eVocabSrc ${evocabsrc} -eVocabTgt ${evocabtgt} -e2fprobs ${e2fprobs} -f2eprobs ${f2eprobs} -fLang ${flang} -eLang ${elang} -fStopWords ${fstopwords} -eStopWords ${estopwords} -fTokens ${ftokens} -eTokens ${etokens} -ein ${epreproc} -fin ${fpreproc}"
+etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.MinhashCLIR -bits ${bits} -k ${k} -M ${samples} -n ${n} -nHash ${nhash} -numReducers 20 -output ${mhoutput} -fVocabSrc ${fvocabsrc} -fVocabTgt ${fvocabtgt} -eVocabSrc ${evocabsrc} -eVocabTgt ${evocabtgt} -e2fprobs ${e2fprobs} -f2eprobs ${f2eprobs} -fLang ${flang} -eLang ${elang} -fStopWords ${fstopwords} -eStopWords ${estopwords} -fTokens ${ftokens} -eTokens ${etokens} -ein ${epreproc} -fin ${fpreproc}
+echo "etc/hadoop-local.sh wikiduper.clir.minhashwiki.MergeClusters -input ${mhoutput} -output ${mergeoutput}"
+etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.MergeClusters -input ${mhoutput} -output ${mergeoutput}
+echo "etc/hadoop-local.sh wikiduper.clir.minhashwiki.GetSentenceClusters -clustermap ${mergeoutput} -elang ${elang} -ewiki ${epreproc} -flang ${flang} -fwiki ${fpreproc} -output ${sentenceoutput}"
+etc/hadoop-cluster.sh wikiduper.clir.minhashwiki.GetSentenceClusters -clustermap ${mergeoutput} -elang ${elang} -ewiki ${epreproc} -flang ${flang} -fwiki ${fpreproc} -output ${sentenceoutput}
 
