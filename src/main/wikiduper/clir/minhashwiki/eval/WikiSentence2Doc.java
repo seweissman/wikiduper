8e41c9b245f25f291dd37c5a3b683dd490421944
Sarah Weissman <seweissman@gmail.com>
2014-01-20 19:08:50 -0500

    removed debugging limits

--- src/main/wikiduper/clir/minhashwiki/eval/WikiSentence2Doc.java
+++ src/main/wikiduper/clir/minhashwiki/eval/WikiSentence2Doc.java
@@ -113,7 +113,7 @@ public class WikiSentence2Doc extends Configured implements Tool {
             Matcher m;
             
             int sentencect = 0;
-            if(Long.parseLong(p.getDocid()) > 100) return;
+
             for(String line: lines){
                 //System.out.println(p.getDocid() + "\n>>>>>>>>CONTENT\n" + line + "\nCONTENT<<<<<<<<<<\n");
                 m = sentenceregex.matcher(line);
@@ -220,7 +220,7 @@ public class WikiSentence2Doc extends Configured implements Tool {
             Matcher m;
             
             int sentencect = 0;
-            if(Long.parseLong(p.getDocid()) > 100) return;
+
             for(String line: lines){
                 //System.out.println(p.getDocid() + "\n>>>>>>>>CONTENT\n" + line + "\nCONTENT<<<<<<<<<<\n");
                 m = sentenceregex.matcher(line);
