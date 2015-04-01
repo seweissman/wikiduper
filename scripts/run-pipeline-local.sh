65066313ae4f0fcdde6c352a51587935e556288c
Sarah Weissman <sweissman@stsci.edu>
2015-04-01 09:11:31 -0400

    Updated build and scripts to allow user to run standard similar sentence detection pipeline with local hadoop

--- scripts/run-pipeline-local.sh
+++ scripts/run-pipeline-local.sh
@@ -8,24 +8,29 @@ lang=en
 # k $5
 # n $6
 # shingleLen $7
+pairs=$1.pairs
+clusters=$1.clusters
+tmppath=tmppath
+wikipack=$1.pack
+mapping=$1.mapping
 
-echo etc/hadoop-local.sh wikiduper.application.MinhashWikipediaPages -wiki_language $lang -input $1 -output enwiki-20130503-pairs -numReducers $nReducers -nHash $3 -k $5 -n $6 -bits $4 -shingleLen $7
-etc/hadoop-local.sh wikiduper.application.MinhashWikipediaPages -wiki_language $lang -input $1 -output enwiki-20130503-pairs -numReducers $nReducers -nHash $3 -k $5 -n $6 -bits $4 -shingleLen $7
-echo etc/hadoop-local.sh wikiduper.application.DedupSentencePairs -input enwiki-20130503-pairs -output enwiki-20130503-pairsdedup
-etc/hadoop-local.sh wikiduper.application.DedupSentencePairs -input enwiki-20130503-pairs -output enwiki-20130503-pairsdedup
-echo rm -rf enwiki-20130503-pairsdedup
-rm -rf enwiki-20130503-pairsdedup
-echo hadoop fs -get enwiki-20130503-pairsdedup
-hadoop fs -get enwiki-20130503-pairsdedup
-echo hadoop fs -rm -r enwiki-20130503-clusters
-hadoop fs -rm -r enwiki-20130503-clusters
-echo etc/run.sh wikiduper.application.MergeClusters -pairfile enwiki-20130503-pairsdedup -output enwiki-20130503-clusters
-etc/run.sh wikiduper.application.MergeClusters -pairfile enwiki-20130503-pairsdedup -output enwiki-20130503-clusters
-echo hadoop fs -put enwiki-20130503-clusters
-hadoop fs -put enwiki-20130503-clusters
-echo etc/hadoop-local.sh wikiduper.application.GetSentenceClusters -input $1 -wiki_language en -clustermap enwiki-20130503-clusters -output $2 -numReducers $nReducers
-etc/hadoop-local.sh wikiduper.application.GetSentenceClusters -input $1 -wiki_language en -clustermap enwiki-20130503-clusters -output $2 -numReducers $nReducers
-echo hadoop fs -get $2 $2-$3-$5-$6-$7-$4
-hadoop fs -get $2 $2-$3-$5-$6-$7-$4
+echo "etc/hadoop-local.sh wikiduper.wikipedia.BuildWikipediaDocnoMapping -input $1 -keep_all -output_file ${emapping} -output_path ${tmppath} -wiki_language ${lang}"
+etc/hadoop-local.sh wikiduper.wikipedia.BuildWikipediaDocnoMapping -input $1 -keep_all -output_file $mapping -output_path $tmppath -wiki_language $lang
+echo "etc/hadoop-local.sh wikiduper.wikipedia.RepackWikipedia -compression_type block -input $1 -mapping_file ${mapping} -output ${wikipack} -wiki_language ${lang}"
+etc/hadoop-local.sh wikiduper.wikipedia.RepackWikipedia -compression_type block -input $1 -mapping_file $mapping -output $wikipack -wiki_language $lang
+echo rm -rf $pairs
+rm -rf $pairs
+echo etc/hadoop-local.sh wikiduper.application.MinhashWikipediaPages -wiki_language $lang -input $wikipack -output $pairs -numReducers $nReducers -nHash $3 -k $5 -n $6 -bits $4 -shingleLen $7
+etc/hadoop-local.sh wikiduper.application.MinhashWikipediaPages -wiki_language $lang -input $wikipack -output $pairs -numReducers $nReducers -nHash $3 -k $5 -n $6 -bits $4 -shingleLen $7
+#echo hadoop fs -get $pairs
+#hadoop fs -get $pairs
+echo rm -rf $clusters
+rm -rf $clusters
+echo etc/run.sh wikiduper.utils.MergeClusters -input $pairs -output $clusters
+etc/run.sh wikiduper.utils.MergeClusters -input $pairs -output $clusters
+echo etc/hadoop-local.sh wikiduper.application.GetSentenceClusters -input $wikipack -wiki_language en -clustermap $clusters -output $2 -numReducers $nReducers
+etc/hadoop-local.sh wikiduper.application.GetSentenceClusters -input $wikipack -wiki_language en -clustermap $clusters -output $2 -numReducers $nReducers
+echo "etc/hadoop-text-local.sh $2/part-* > $2-$3-$5-$6-$7-$4"
+etc/hadoop-text-local.sh $2/part-* > $2-$3-$5-$6-$7-$4
 #echo mv $2-$3-$5-$6-$7-$4 /scratch0/sew
 #mv $2-$3-$5-$6-$7-$4 /scratch0/sew
